09ae06fe17a0f9b41afae9bc101a34808a59bddb
Ed Blom <edblom@gmail.com>
2025-04-01 21:46:00 +0200

    Acties api toegeveogd

---
+++ KlantBaseWebDemo/Components/Pages/TblMemosApi.razor.cs
@@ -0,0 +1,121 @@
+using KlantBaseWebDemo.Models.KlantBase;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Radzen;
+using Radzen.Blazor;
+using System;
+using System.Collections.Generic;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace KlantBaseWebDemo.Components.Pages
+{
+    [Authorize]
+    public class TblMemosApiBase : ComponentBase
+    {
+        [Inject] protected IHttpClientFactory HttpClientFactory { get; set; }
+        [Inject] protected NavigationManager Navigation { get; set; }
+        [Inject] protected NotificationService NotificationService { get; set; }
+
+        protected RadzenDataGrid<TblMemo> grid0;
+        protected IEnumerable<TblMemo> tblMemos;
+        protected HttpClient Http => HttpClientFactory.CreateClient("KlantBaseApi");
+
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadData();
+        }
+
+        protected async Task LoadData()
+        {
+            try
+            {
+                tblMemos = await Http.GetFromJsonAsync<List<TblMemo>>("api/acties");
+                if (tblMemos == null || !tblMemos.Any())
+                {
+                    NotificationService.Notify(new NotificationMessage
+                    {
+                        Severity = NotificationSeverity.Warning,
+                        Summary = "Geen data",
+                        Detail = "Geen acties gevonden."
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                NotificationService.Notify(new NotificationMessage
+                {
+                    Severity = NotificationSeverity.Error,
+                    Summary = "Fout bij laden",
+                    Detail = ex.Message
+                });
+            }
+        }
+
+        protected void EditRow(TblMemo item)
+        {
+            Navigation.NavigateTo($"/tbl-memos-api/edit/{item.FldMid}");
+        }
+
+        protected async Task AddButtonClick(MouseEventArgs args)
+        {
+            var newMemo = new TblMemo
+            {
+                FldOmschrijving = "Nieuwe actie",
+                FldMactieDatum = DateTime.Now,
+                FldMprioriteit = 1,
+                FldMcontactPers = "",
+                FldMafspraak = "",
+                FldMactieSoort = ""
+            };
+
+            var response = await Http.PostAsJsonAsync("api/acties", newMemo);
+            if (response.IsSuccessStatusCode)
+            {
+                await LoadData();
+                await grid0.Reload();
+                NotificationService.Notify(new NotificationMessage
+                {
+                    Severity = NotificationSeverity.Success,
+                    Summary = "Toegevoegd",
+                    Detail = "Actie succesvol toegevoegd."
+                });
+            }
+            else
+            {
+                NotificationService.Notify(new NotificationMessage
+                {
+                    Severity = NotificationSeverity.Error,
+                    Summary = "Fout bij toevoegen",
+                    Detail = $"Status: {response.StatusCode}"
+                });
+            }
+        }
+
+        protected async Task GridDeleteButtonClick(MouseEventArgs args, TblMemo tblMemo)
+        {
+            var response = await Http.DeleteAsync($"api/acties/{tblMemo.FldMid}");
+            if (response.IsSuccessStatusCode)
+            {
+                await LoadData();
+                await grid0.Reload();
+                NotificationService.Notify(new NotificationMessage
+                {
+                    Severity = NotificationSeverity.Success,
+                    Summary = "Verwijderd",
+                    Detail = "Actie succesvol verwijderd."
+                });
+            }
+            else
+            {
+                NotificationService.Notify(new NotificationMessage
+                {
+                    Severity = NotificationSeverity.Error,
+                    Summary = "Fout bij verwijderen",
+                    Detail = $"Status: {response.StatusCode}"
+                });
+            }
+        }
+    }
+}
--- KlantBaseWebDemo/Program.cs
+++ KlantBaseWebDemo/Program.cs
@@ -9,8 +9,12 @@ using Microsoft.OData.ModelBuilder;
 using Microsoft.AspNetCore.Components.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
+
 // Add services to the container.
-builder.Services.AddRazorComponents().AddInteractiveServerComponents().AddHubOptions(options => options.MaximumReceiveMessageSize = 10 * 1024 * 1024);
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddHubOptions(options => options.MaximumReceiveMessageSize = 10 * 1024 * 1024);
+
 builder.Services.AddControllers();
 builder.Services.AddRadzenComponents();
 builder.Services.AddRadzenCookieThemeService(options =>
@@ -18,14 +22,25 @@ builder.Services.AddRadzenCookieThemeService(options =>
     options.Name = "KlantBaseWebDemoTheme";
     options.Duration = TimeSpan.FromDays(365);
 });
-builder.Services.AddHttpClient();
+
+// Configureer HttpClient voor KlantBase API zonder authenticatie
+builder.Services.AddHttpClient("KlantBaseApi", client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["KlantBaseApiUrl"] ?? "https://jouw-api-url/");
+    client.DefaultRequestHeaders.Add("Accept", "application/json");
+});
+
+// Bestaande services
 builder.Services.AddScoped<KlantBaseWebDemo.KlantBaseService>();
 builder.Services.AddDbContext<KlantBaseWebDemo.Data.KlantBaseContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("KlantBaseConnection"));
 });
-builder.Services.AddHttpClient("KlantBaseWebDemo").ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler { UseCookies = false }).AddHeaderPropagation(o => o.Headers.Add("Cookie"));
+builder.Services.AddHttpClient("KlantBaseWebDemo")
+    .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler { UseCookies = false })
+    .AddHeaderPropagation(o => o.Headers.Add("Cookie"));
 builder.Services.AddHeaderPropagation(o => o.Headers.Add("Cookie"));
+
 builder.Services.AddAuthentication();
 builder.Services.AddAuthorization();
 builder.Services.AddScoped<KlantBaseWebDemo.SecurityService>();
@@ -33,7 +48,10 @@ builder.Services.AddDbContext<ApplicationIdentityDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("KlantBaseConnection"));
 });
-builder.Services.AddIdentity<ApplicationUser, ApplicationRole>().AddEntityFrameworkStores<ApplicationIdentityDbContext>().AddDefaultTokenProviders();
+builder.Services.AddIdentity<ApplicationUser, ApplicationRole>()
+    .AddEntityFrameworkStores<ApplicationIdentityDbContext>()
+    .AddDefaultTokenProviders();
+
 builder.Services.AddControllers().AddOData(o =>
 {
     var oDataBuilder = new ODataConventionModelBuilder();
@@ -42,27 +60,35 @@ builder.Services.AddControllers().AddOData(o =>
     usersType.AddProperty(typeof(ApplicationUser).GetProperty(nameof(ApplicationUser.Password)));
     usersType.AddProperty(typeof(ApplicationUser).GetProperty(nameof(ApplicationUser.ConfirmPassword)));
     oDataBuilder.EntitySet<ApplicationRole>("ApplicationRoles");
-    o.AddRouteComponents("odata/Identity", oDataBuilder.GetEdmModel()).Count().Filter().OrderBy().Expand().Select().SetMaxTop(null).TimeZone = TimeZoneInfo.Utc;
+    o.AddRouteComponents("odata/Identity", oDataBuilder.GetEdmModel())
+        .Count().Filter().OrderBy().Expand().Select().SetMaxTop(null).TimeZone = TimeZoneInfo.Utc;
 });
+
 builder.Services.AddScoped<AuthenticationStateProvider, KlantBaseWebDemo.ApplicationAuthenticationStateProvider>();
 builder.Services.AddLocalization();
+
 var app = builder.Build();
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.MapControllers();
 app.UseHeaderPropagation();
-app.UseRequestLocalization(options => options.AddSupportedCultures("nl-NL", "en", "da-DK").AddSupportedUICultures("nl-NL", "en", "da-DK").SetDefaultCulture("nl-NL"));
+app.UseRequestLocalization(options => options.AddSupportedCultures("nl-NL", "en", "da-DK")
+    .AddSupportedUICultures("nl-NL", "en", "da-DK")
+    .SetDefaultCulture("nl-NL"));
 app.UseStaticFiles();
 app.UseAuthentication();
 app.UseAuthorization();
 app.UseAntiforgery();
+
 app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
+
 app.Services.CreateScope().ServiceProvider.GetRequiredService<ApplicationIdentityDbContext>().Database.Migrate();
+
 app.Run();
