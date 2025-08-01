8c7928d393bb562c38bcc7175b9dd5ed5d227ad1
Ed Blom <edblom@gmail.com>
2025-08-01 14:17:11 +0200

    Fixed Dialog and notification

---
+++ KlantBaseWASM/wwwroot/js/interop.js
@@ -0,0 +1,16 @@
+﻿window.blazorTest = function () {
+    console.log("blazorTest called from JS");
+    return "Hello from JavaScript!";
+};
+
+window.checkDialogExists = function () {
+    const dialog = document.querySelector('.rz-dialog');
+    console.log('Dialog check result:', dialog ? true : false);
+    return dialog ? true : false;
+};
+
+window.checkNotificationExists = function () {
+    const notification = document.querySelector('.rz-notification');
+    console.log('Notification check result:', notification ? true : false);
+    return notification ? true : false;
+};
---
+++ RadzenTestWASM/wwwroot/js/interop.js
@@ -0,0 +1,5 @@
+﻿window.checkNotificationExists = function () {
+    const notification = document.querySelector('.rz-notification');
+    console.log('Notification check result:', notification ? true : false);
+    return notification ? true : false;
+};
